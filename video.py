cfde2bca7ae570a1d01d3e27e3f07b3c194b0cef
leomoraes23 <leo.moraes23@live.com>
2018-06-27 17:41:57 -0300

    Update video.py

--- video.py
+++ video.py
@@ -16,7 +16,7 @@ async def on_ready():
     print("Iniciando client.......")
     print("Iniciando client.........")
     print("BOT iniciado com sucesso!")
-    await client.change_presence(game=discord.Game(name="Não, comprando aparatos sexuais", type=1))
+    await client.change_presence(game=discord.Game(name="Não, comprando aparatos sexuais", type=4))
 
 
 @client.event
