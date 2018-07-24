aea71f8b380e3ecec0a932d728f82a7b55a8957c
leomoraes23 <leo.moraes23@live.com>
2018-07-23 23:38:43 -0300

    Update video.py

--- video.py
+++ video.py
@@ -34,7 +34,7 @@ async def on_member_join(member):
 @client.event
 async def on_message(message):
     
-        if message.content.lower().startswith('!sabio'):
+    if message.content.lower().startswith('tl!sabio'):
         asyncio.sleep(0.1)
         await client.delete_message(message)
 
